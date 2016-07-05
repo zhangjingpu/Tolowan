1ca29bbcd5325d6f157ddd61683901c743c0f466
Leno <leno@itdashu.com>
2016-07-05 21:46:21 +0800

    更新默认管理员账户

--- tolowan.sql
+++ tolowan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: 2016-07-03 08:05:31
--- 服务器版本： 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Host: 127.0.0.1
+-- Generation Time: 2016-07-05 13:27:12
+-- 服务器版本： 10.1.13-MariaDB
+-- PHP Version: 5.6.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -80,6 +80,13 @@ CREATE TABLE `node` (
   `comment_num` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 转存表中的数据 `node`
+--
+
+INSERT INTO `node` (`id`, `state`, `contentModel`, `created`, `changed`, `uid`, `comment`, `top`, `essence`, `hot`, `love`, `browse`, `comment_num`) VALUES
+(2, 1, 'article', 1467638278, 1467638278, 0, 0, 0, 0, 0, 0, 0, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -93,6 +100,13 @@ CREATE TABLE `node_field_body` (
   `full_text` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 转存表中的数据 `node_field_body`
+--
+
+INSERT INTO `node_field_body` (`id`, `eid`, `value`, `full_text`) VALUES
+(2, 2, '<p>Tolowan</p>\n\n<p>是基于Phalcon开发的内容管理系统。ps：现在程序并未完善，仅供把玩，请勿用于生产环境 特性：</p>\n\n<blockquote>\n<ul>\n	<li>继承Phalcon框架全功能</li>\n	<li>多网站支持，异站点用户文件、同网站私有/共有网站隔离</li>\n	<li>强大的个性化环境，每个用户可以对网站内容和表现形式进行个性化设置</li>\n	<li>基于用户角色、模块、角色的权限控制系统，当然，您也可以通过回调函数进行更精细控制</li>\n	<li>提供的站内搜索系统原生支持全文搜索。</li>\n	<li>使用volt编写主题模板，类twig语法，单比twig更高效</li>\n	<li>Tolowan提供的实体管理、字段管理、表单管理、模型管理等机制，可以大大缩减二次开发的难度和所需时间</li>\n</ul>\n</blockquote>\n\n<hr />\n<h2>安装phalcon</h2>\n\n<p>前往phalcon官网按步骤安装phalcon扩展：<a href="https://phalconphp.com/zh/download">https://phalconphp.com/zh/download</a></p>\n\n<h2>安装Tolowan</h2>\n\n<p>下载安装包解压至web目录</p>\n\n<h3>1. 修改site.php文件</h3>\n\n<p>编辑siteroot/Web/site.php文件，将需要绑定的域名根据文件中格式录入</p>\n\n<h3>2. 复制public目录</h3>\n\n<p>例如在第一步中，我们录入的域名为：baidu.com -&gt; Baidu ,我们则需要将siteroot/public目录复制为siteroot/Baidu 注意：首字母必须未大写</p>\n\n<h3>3. 复制site/default目录</h3>\n\n<p>例如在第一步中，我们录入的域名为：baidu.com -&gt; Baidu ,我们则需要将siteroot/Web/default目录复制为siteroot/Web/Baidu 注意：首字母必须未大写</p>\n\n<h3>4. 绑定域名</h3>\n\n<p>根据上文设置，以apache服务器为例，需要进行如下设置</p>\n\n<pre>\n\nDocumentRoot siteroot/Baidu\nServerName baidu.com\n\n</pre>\n\n<blockquote>\n<p>注：其中上文中siteroot为程序目录所在地址，QQ交流群：574199144</p>\n</blockquote>\n', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -105,6 +119,13 @@ CREATE TABLE `node_field_cat` (
   `value` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 转存表中的数据 `node_field_cat`
+--
+
+INSERT INTO `node_field_cat` (`id`, `eid`, `value`) VALUES
+(2, 2, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -114,8 +135,15 @@ CREATE TABLE `node_field_cat` (
 CREATE TABLE `node_field_images` (
   `id` int(11) NOT NULL,
   `eid` int(11) NOT NULL,
-  `value` varchar(255) NOT NULL
+  `value` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `node_field_images`
+--
+
+INSERT INTO `node_field_images` (`id`, `eid`, `value`) VALUES
+(1, 2, '');
 
 -- --------------------------------------------------------
 
@@ -129,6 +157,13 @@ CREATE TABLE `node_field_tags` (
   `value` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 转存表中的数据 `node_field_tags`
+--
+
+INSERT INTO `node_field_tags` (`id`, `eid`, `value`) VALUES
+(2, 2, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -141,6 +176,13 @@ CREATE TABLE `node_field_title` (
   `value` varchar(255) NOT NULL,
   `full_text` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `node_field_title`
+--
+
+INSERT INTO `node_field_title` (`id`, `eid`, `value`, `full_text`) VALUES
+(2, 2, '欢迎使用Tolowan', NULL);
 
 -- --------------------------------------------------------
 
@@ -174,6 +216,14 @@ CREATE TABLE `term` (
   `contentModel` varchar(32) NOT NULL,
   `attach` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `term`
+--
+
+INSERT INTO `term` (`id`, `name`, `description`, `parent`, `widget`, `other`, `contentModel`, `attach`) VALUES
+(1, '未分类', '未分类', 0, 10, NULL, 'cat', NULL),
+(3, 'phalcon', 'phalcon', 0, 10, NULL, 'tags', NULL);
 
 -- --------------------------------------------------------
 
@@ -334,42 +384,42 @@ ALTER TABLE `file`
 -- 使用表AUTO_INCREMENT `node`
 --
 ALTER TABLE `node`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- 使用表AUTO_INCREMENT `node_field_body`
 --
 ALTER TABLE `node_field_body`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- 使用表AUTO_INCREMENT `node_field_cat`
 --
 ALTER TABLE `node_field_cat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- 使用表AUTO_INCREMENT `node_field_images`
 --
 ALTER TABLE `node_field_images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- 使用表AUTO_INCREMENT `node_field_tags`
 --
 ALTER TABLE `node_field_tags`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- 使用表AUTO_INCREMENT `node_field_title`
 --
 ALTER TABLE `node_field_title`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- 使用表AUTO_INCREMENT `queue`
 --
 ALTER TABLE `queue`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- 使用表AUTO_INCREMENT `term`
 --
 ALTER TABLE `term`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- 使用表AUTO_INCREMENT `user`
 --
